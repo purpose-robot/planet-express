536b9d5945bf13c6b5dd85ede307fc548f256d77
Stefan Maurer <s.maurer@bechtle.com>
2026-03-19 18:21:33 +0100

    chore: Remove error kind

--- internal/cisco/errors.go
+++ internal/cisco/errors.go
@@ -9,13 +9,12 @@ import (
 var (
 	ErrDuplicateAuthMethod = errors.New("credentials already exists")
 	// The following errors are used to provide better error handling with SSH.
-	ErrDiscovererCommandFailed        = errors.New("discoverer: command failed")
-	ErrDiscovererInvalidOptions       = errors.New("discoverer: invalid options")
-	ErrDiscovererConnectionLost       = errors.New("discoverer: connection lost")
-	ErrDiscovererConnectionFailed     = errors.New("discoverer: failed to connect")
-	ErrDiscovererInvalidTemplate      = errors.New("discoverer: invalid template")
-	ErrDiscovererUnexpectedOutput     = errors.New("discoverer: unexpected output")
-	ErrDiscovererAuthenticationFailed = errors.New("discoverer: authentication failed")
+	ErrDiscovererCommandFailed    = errors.New("discoverer: command failed")
+	ErrDiscovererInvalidOptions   = errors.New("discoverer: invalid options")
+	ErrDiscovererConnectionLost   = errors.New("discoverer: connection lost")
+	ErrDiscovererConnectionFailed = errors.New("discoverer: failed to connect")
+	ErrDiscovererInvalidTemplate  = errors.New("discoverer: invalid template")
+	ErrDiscovererUnexpectedOutput = errors.New("discoverer: unexpected output")
 )
 
 func mapDomainError(err error) error {
